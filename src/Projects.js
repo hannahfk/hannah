d88d54b8e0f352513444ef6e9f219263a455dce9
Hannah Kemp <hfk@mail.usf.edu>
2021-09-26 20:35:59 -0400

    change to projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./Projects.css";
-import donut from "./images/Donut-clipart copy.png"
-import unicorn from './images/glow copy.png'
-import ticket from './images/ticket2.jpeg'
+import donut from "./images/Donut-clipart copy.png";
+import unicorn from "./images/glow copy.png";
+import ticket from "./images/ticket2.jpeg";
 
 const Projects = (props) => {
   return (
     <div className="projects">
-       <div className="sign">
-      <span className="fast-flicker">P</span>roje
-      <span className="flicker">ct</span>s
-    </div>
+      <div className="sign">
+        <span className="fast-flicker">P</span>roje
+        <span className="flicker">ct</span>s
+      </div>
       <div className="skill-row">
-       
-        <img src={unicorn}  className="design" alt="origami unicorn" />
+        <img src={unicorn} className="design" alt="origami unicorn" />
         <h3>Glow In The Blue</h3>
         <p>"A Cozy Place For Pacing Your Day"</p>
         <p>
@@ -22,19 +21,22 @@ const Projects = (props) => {
           Express, PostgreSQL
         </p>
         <p>
-          
           <div id="neonShadow">
-          <button className="project-button" onClick={(e) => {
-      e.preventDefault();
-      window.location.href='http://glowintheblue.herokuapp.com/';
-      }}>Go ➪</button>
-          
+            <button
+              className="project-button"
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = "http://glowintheblue.herokuapp.com/";
+              }}
+            >
+              Go ➪
+            </button>
           </div>
         </p>
       </div>
       <hr></hr>
       <div className="skill-row">
-      <img src={donut}  className="coding" alt="donut" />
+        <img src={donut} className="coding" alt="donut" />
         <h3>Squats Til Donuts</h3>
         <p>
           A fitness inspired app the number of squats you do and reminds you to
@@ -42,31 +44,40 @@ const Projects = (props) => {
           PostgreSQL, Sequelize, NodeJS.
         </p>
         <div id="neonShadow">
-        <button className="project-button" onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://expo.io/@hannahkemp/projects/squats';
-      }}>Go ➪</button>
+          <button
+            className="project-button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href =
+                "https://expo.io/@hannahkemp/projects/squats";
+            }}
+          >
+            Go ➪
+          </button>
         </div>
       </div>
-      
+
       <hr></hr>
       <div className="skill-row">
-      <img src={ticket}  className="design" alt="ticket" />
+        <img src={ticket} className="design" alt="ticket" />
         <h3>City Tickets NYC</h3>
         <p>
           An e-commerce site listing and selling tickets to events in New York
           City. Created using React, Redux, Express, PostgreSQL, Sequelize,
           NodeJS.
         </p>
-      
-          <div id="neonShadow">
-          <button className="project-button" onClick={(e) => {
-      e.preventDefault();
-      window.location.href='https://cityticketsnyc.herokuapp.com';
-      }}>Go ➪</button>
-          
-          </div>
-     
+
+        <div id="neonShadow">
+          <button
+            className="project-button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "https://cityticketsnyc.herokuapp.com";
+            }}
+          >
+            Go ➪
+          </button>
+        </div>
       </div>
     </div>
   );
